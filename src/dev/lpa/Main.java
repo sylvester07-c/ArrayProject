68fbd850a19fbd6485f1ae0ab199a51ed30bb9f9
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-05 07:34:33 +0100

    updates

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -26,33 +26,59 @@ public class Main {
             flag = false;
             for(int i = 0; i < sortedArrays.length - 1; i++){
                 if(sortedArrays[i] < sortedArrays[i + 1]){
+
                     temp = sortedArrays[i];
-                    sortedArrays[i] = sortedArrays[i + 1];
-                    sortedArrays[i + 1] = temp;
-                    flag = true;
-                }
+
+                sortedArrays[i + 1] = temp;                          }                    sortedArrays[i] = sortedArrays[i + 1];
+                flag = true;
             }
         }
         return  sortedArrays;
     }
 
-    public static int[] getInteger(int len) {
+    public class SortedArray {
 
-        int[] arrayNumbers = new int[len];
-        for (int i = 0; i < len; i++) {
+        public static int[] getIntegers(int capacity) {
+
             Scanner scanner = new Scanner(System.in);
-            String num = scanner.nextLine();
-            arrayNumbers[i] = Integer.parseInt(num);
+            int[] array = new int[capacity];
+            System.out.println("Enter " + capacity + " integer values:\r");
+            for (int i=0; i<array.length; i++) {
+                array[i] = scanner.nextInt();
+            }
+            return array;
         }
 
-        return arrayNumbers;
+        public static void printArray(int[] array) {
 
+            for (int i=0; i<array.length; i++) {
+                System.out.println("Element " + i + " contents " + array[i]);
+            }
+        }
 
+        public static int[] sortIntegers(int[] array) {
+
+            int[] sortedArray = Arrays.copyOf(array, array.length);
+            boolean flag = true;
+            int temp;
+            while (flag) {
+                flag = false;
+                for (int i=0; i<sortedArray.length-1; i++) {
+                    if (sortedArray[i] < sortedArray[i+1]) {
+                        temp = sortedArray[i];                     // 1
+                        sortedArray[i] = sortedArray[i+1];         // 2
+                        sortedArray[i+1] = temp;                   // 3
+                        flag = true;                               // 4
+                    }
+                }
+            }
+            return sortedArray;
+        }
     }
 
-
     public static void main(String[] args) {
-        System.out.println(getInteger(10));
+
+//        System.out.println(Arrays.toString(numbers));
 //        int[] randomNumbers = randomArray(10);
 //        Arrays.sort(randomNumbers);
 //        System.out.println(Arrays.toString(randomNumbers));
