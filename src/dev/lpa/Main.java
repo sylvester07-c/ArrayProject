acdd51c42357e81390a5f9018f8d8f07e632fb9a
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-06 22:25:14 +0100

    updates

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,37 +5,37 @@ import java.util.Scanner;
 public class Main {
 
 
-    public class MinimumElement {
-        // write code here
-        private static int readIntegers() {
+
+        public static int readIntegers() {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a group of numbers seperated by a comma:");
+            System.out.println("Enter the number of elements you wish to enter? ");
             String input = scanner.nextLine();
             int value = Integer.parseInt(input);
 
             return value;
         }
 
-        private static int[] readElements(int value) {
-            value = readIntegers();
-            int count = 0;
-
+        public static int[] readElements(int value) {
 
                 Scanner scanner = new Scanner(System.in);
-                System.out.println("Enter " + value + "number of integers: ");
-                String numbers = scanner.nextLine();
+                int count = 0;
+                int[] values = new int[value];
+                while(count < value) {
+                    System.out.println("Enter " + value + " number of integers: ");
+                    String numbers = scanner.nextLine();
 
-                String[] splits = numbers.split(",");
-                int[] values = new int[splits.length];
-                for(int i = 0; i < splits.length; i++) {
-                    values[i] = Integer.parseInt(splits[i].trim());
+                    String[] splits = numbers.split(",");
+                     values = new int[splits.length];
+                    for (int i = 0; i < splits.length; i++) {
+                        values[i] = Integer.parseInt(splits[i].trim());
+                    }
                 }
-                count++;
 
-            return values;
+                    return values;
+
         }
 
-        private static int findMin(int[] arr) {
+        public static int findMin(int[] arr) {
             int min = arr[0];
             for(int i = 0; i < arr.length; i++) {
                 if(i < min) {
@@ -45,9 +45,13 @@ public class Main {
 
             return min;
         }
-    }
+
 
     public static void main(String[] args) {
+            int value = readIntegers();
+
+            System.out.println(readElements(value));
+
 
     }
 }
