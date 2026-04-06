d017fac3830be41d52348e53844109877638a430
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-06 22:29:35 +0100

    updates

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -50,8 +51,11 @@ public class Main {
 
     public static void main(String[] args) {
             int value = readIntegers();
+            int[] arr = readElements(value);
 
-            System.out.println(readElements(value));
+
+            System.out.println(Arrays.toString(readElements(value)));
+            System.out.println("Min value is: " + findMin(arr));
 
 
     }
